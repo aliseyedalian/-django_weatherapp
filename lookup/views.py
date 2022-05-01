4647a7bce415eeb24fa76b9d37de84950dc5b527
SASeyedalian <s.ali.seyedalian@gmail.com>
2022-05-01 12:38:21 +0430

    some tweaks

--- lookup/views.py
+++ lookup/views.py
@@ -7,7 +7,7 @@ import requests
 # Create your views here.
 def home(request):
     # key: 12678f1a5ce946ba929185409223004
-    city = "Birjand"
+    city = "Tehran"
     api_request = requests.get("http://api.worldweatheronline.com/premium/v1/weather.ashx?key=12678f1a5ce946ba929185409223004&q={}&fx=no&cc=no&mca=yes&format=json".format(city))  
     try:
         api = json.loads(api_request.content)
